1e7e15a3b50d9d76cec057d5b2e9e5643eb83acd
tenyears <comexie@yahoo.com>
2018-10-10 14:32:25 +0800

    Initial commit

---
+++ DownloadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadDemo
+//
+//  Created by yuqian on 2018/10/10.
+//  Copyright © 2018年 yuqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
